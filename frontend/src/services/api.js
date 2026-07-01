9186ed9d4cc75744a51c6eb978e09b3d1f16af42
akash-4x3 <akashkmrsn0123@gmail.com>
2026-07-01 14:06:49 +0530

    Changed api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,19 +1,9 @@
 import axios from "axios";
 
+console.log("API URL:", import.meta.env.VITE_API_BASE_URL);
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
-});
-
-api.interceptors.request.use((config) => {
-
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
-
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 export default api;
